45d139d014b1843ce5b427c69dfeac801a922bfc
bpaplow <bpaplow@tcmaker.org>
2021-06-19 17:11:07 -0500

    Update footer.php
    fix irc link

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -22,7 +22,7 @@
           <li><a href="https://github.com/tcmaker">GitHub</a></li>
           <li><a href="https://groups.google.com/forum/#!forum/tcmaker">Google Groups</a></li>
           <li><a href="https://twitter.com/tcmaker">Twitter</a></li>
-          <li><a href="https://webchat.freenode.net/?channels=%23tcmaker&uio=d4">Freenode IRC</a></li>
+          <li><a href="https://web.libera.chat/#tcmaker">Libera.chat IRC</a></li>
           <li><a href="https://tcmaker.slack.com/">Slack</a></li>
         </ul>
       </div>
