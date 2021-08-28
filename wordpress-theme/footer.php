be8b58dd333d37970c258a8192b1e772992cc450
automatingerror <38846936+automatingerror@users.noreply.github.com>
2021-08-28 15:09:23 -0500

    Update footer.php

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -31,7 +31,7 @@
         <p><img src="<?php echo image_uri('logo-white-text.svg'); ?>"></p>
         <p>Copyright &copy; 2009&ndash;<?php echo date('Y'); ?> Twin Cities Maker.</p>
         <ul class="list-unstyled">
-          <a href="https://apps.irs.gov/app/eos/detailsPage?ein=271536940&amp;name=Twin%20Cities%20Maker%20Inc.&amp;city=Minneapolis&amp;state=MN&amp;countryAbbr=US&amp;dba=&amp;type=CHARITIES,%20DETERMINATIONLETTERS,%20EPOSTCARD,%20REVOCATION,%20COPYOFRETURNS&amp;orgTags=CHARITIES&amp;orgTags=DETERMINATIONLETTERS&amp;orgTags=EPOSTCARD&amp;orgTags=REVOCATION&amp;orgTags=COPYOFRETURNS">IRS 501(c)3 Exeption Filings</a>
+          <li><a href="https://apps.irs.gov/app/eos/detailsPage?ein=271536940&amp;name=Twin%20Cities%20Maker%20Inc.&amp;city=Minneapolis&amp;state=MN&amp;countryAbbr=US&amp;dba=&amp;type=CHARITIES,%20DETERMINATIONLETTERS,%20EPOSTCARD,%20REVOCATION,%20COPYOFRETURNS&amp;orgTags=CHARITIES&amp;orgTags=DETERMINATIONLETTERS&amp;orgTags=EPOSTCARD&amp;orgTags=REVOCATION&amp;orgTags=COPYOFRETURNS">IRS 501(c)3 Exeption Filings</a> </li>
           <li><a href="https://mblsportal.sos.state.mn.us/Business/SearchDetails?filingGuid=7e9eccc8-94d4-e011-a886-001ec94ffe7f"> MN Secretary of State Filings</a> </li>
           <li><a href="https://www.ag.state.mn.us/Charity/Search/CHR_GeneralInfo.asp?FederalID=271536940&Yr=CURR&cmdSearch=View"> MN Attorney General Charity Search</a> </li>
           <li><a href="https://www.guidestar.org/profile/27-1536940">GuideStar Page</a></li>
